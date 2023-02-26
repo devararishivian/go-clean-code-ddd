b7d8ff83d2faa4c34f2ac1c03386133cf775bfd6
Devvara Rishivian <rishivian@gmail.com>
2023-02-26 20:04:31 +0700

    feat: using connection pool

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/gofiber/fiber/v2 v2.42.0
-	github.com/jackc/pgx/v4 v4.18.0
+	github.com/jackc/pgx/v5 v5.3.0
 	github.com/spf13/viper v1.15.0
 )
 
@@ -13,13 +13,9 @@ require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.14.0 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.3.2 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -41,6 +37,7 @@ require (
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
--- internal/infrastructure/database.go
+++ internal/infrastructure/database.go
@@ -3,17 +3,18 @@ package infrastructure
 import (
 	"context"
 	"fmt"
-	appConfig "github.com/devararishivian/antrekuy/internal/config"
-	"github.com/jackc/pgx/v4"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	appConfig "github.com/devararishivian/antrekuy/internal/config"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Database struct {
-	Conn *pgx.Conn
+	Conn *pgxpool.Pool
 }
 
 func NewDatabase() (*Database, error) {
@@ -29,29 +30,29 @@ func NewDatabase() (*Database, error) {
 	)
 
 	// Define the database configuration
-	config, err := pgx.ParseConfig(dsn)
+	config, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse database config: %w", err)
 	}
 
 	// Connect to the database
-	conn, err := pgx.ConnectConfig(ctx, config)
+	dbpool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
 
 	// Create a new database instance
 	db := &Database{
-		Conn: conn,
+		Conn: dbpool,
 	}
 
 	// Register a signal handler to gracefully close the database connection when the application shuts down
-	go db.gracefulShutdown()
+	go db.gracefulShutdown(ctx)
 
 	return db, nil
 }
 
-func (db *Database) gracefulShutdown() {
+func (db *Database) gracefulShutdown(ctx context.Context) {
 	// Create a channel to listen for OS signals
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
@@ -59,12 +60,20 @@ func (db *Database) gracefulShutdown() {
 	// Wait for a signal
 	<-c
 
-	// Start a context with a timeout
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
+	log.Println("Shutting down database connection...")
 
 	// Close the database connection
-	log.Println(db.Conn.Close(ctx))
+	db.Conn.Close()
+
+	// Cancel the context to release any idle connections held by the pool
+	cancelFunc := func() {}
+	if deadline, ok := ctx.Deadline(); ok {
+		timeout := time.Until(deadline)
+		ctx, cancelFunc = context.WithTimeout(ctx, timeout)
+	}
+	cancelFunc()
+
+	log.Println("Database connection closed")
 
 	os.Exit(0)
 }
