76e3f715ca8b1635421afd5cf9686b64e28d07d7
Devvara Rishivian <rishivian@gmail.com>
2023-02-26 15:02:56 +0700

    refactor: reorganize routes registration

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,21 @@
 package main
 
 import (
-	"github.com/devararishivian/antrekuy/internal/application/usecase"
 	"github.com/devararishivian/antrekuy/internal/infrastructure"
-	"github.com/devararishivian/antrekuy/internal/infrastructure/persistence"
 	"github.com/devararishivian/antrekuy/internal/interface/api"
 	"github.com/gofiber/fiber/v2"
 	"log"
 )
 
 func main() {
-	// Create a new database connection
 	db, err := infrastructure.NewDatabase()
 	if err != nil {
 		panic(err)
 	}
 
-	userRepository := persistence.NewUserRepository(db)
-	userUseCase := usecase.NewUserUseCase(userRepository)
-
 	app := fiber.New()
-	apiPrefixV1 := app.Group("api/v1")
 
-	api.RegisterUserRoutes(apiPrefixV1, userUseCase)
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	api.RegisterRoutes(app.Group("api"), db)
 
 	log.Fatal(app.Listen(":3000"))
 }
--- internal/interface/api/routes.go
+++ internal/interface/api/routes.go
@@ -1,13 +1,27 @@
 package api
 
 import (
-	"github.com/devararishivian/antrekuy/internal/domain/service"
+	"github.com/devararishivian/antrekuy/internal/application/usecase"
+	"github.com/devararishivian/antrekuy/internal/infrastructure"
+	"github.com/devararishivian/antrekuy/internal/infrastructure/persistence"
 	"github.com/gofiber/fiber/v2"
 )
 
-// RegisterUserRoutes registers user-related routes
-func RegisterUserRoutes(router fiber.Router, userUseCase service.UserService) {
-	handler := NewUserHandler(userUseCase)
+func RegisterRoutes(router fiber.Router, db *infrastructure.Database) {
+	router.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 
-	router.Get("/user", handler.Store)
+	v1Route := router.Group("v1")
+
+	registerUserRoutesV1(v1Route, db)
+}
+
+func registerUserRoutesV1(router fiber.Router, db *infrastructure.Database) {
+	repository := persistence.NewUserRepository(db)
+	useCase := usecase.NewUserUseCase(repository)
+	handler := NewUserHandler(useCase)
+
+	userRoute := router.Group("user")
+	userRoute.Get("/", handler.Store)
 }
